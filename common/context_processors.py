028a342475d8efac5ca9c8dbe7555771b940ce0b
Abdullah AKTAŞ <51528934+abdullah-aktas@users.noreply.github.com>
2025-12-14 18:23:57 +0300

    Fix address: change back to zmir Aliağa, Türkiye

--- common/context_processors.py
+++ common/context_processors.py
@@ -48,7 +48,7 @@ BRAND_IDENTITY = {
         "email": "destek@finasis.com.tr",
         "phone": "+90 850 000 00 00",  # TODO: Gerçek telefon numarası ile değiştirilecek
         "hours": "Hafta içi 09:00 - 18:00",
-        "address": "İstanbul, Türkiye",
+        "address": "İzmir Aliağa, Türkiye",
         "slack_channel": "https://finasis.slack.com/support",
     },
     "assets": {
